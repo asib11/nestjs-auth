df329c3a4f5e55b22af92563735506dbd458d661
Asib <ahmedasib1@gmail.com>
2025-03-24 00:35:26 +0600

    feat #4 : passport strategies

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: 'rt-secret',
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    const refreshToken = req.get('authorization')?.split(' ')[1];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
