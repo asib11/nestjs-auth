53217730864853d6525efef9d2a861e2265c374c
Asib <ahmedasib1@gmail.com>
2025-03-26 01:30:11 +0600

    feat #5 : nest jwt accessToken and refreshToken setup complete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,42 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   hashData(data: string) {
     return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string) {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+    return { accessToken: at, refreshToken: rt };
   }
 
   async signupLocal(dto: AuthDto): Promise<Tokens> {
@@ -19,7 +49,9 @@ export class AuthService {
         hash,
       },
     });
-    // return newUser;
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+
+    return tokens;
   }
   signinLocal() {}
   logout() {}
